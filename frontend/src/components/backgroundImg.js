da4d9980cc8b924a9e7f721ab046d33672fce069
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2024-01-01 18:00:20 +0100

    img

--- frontend/src/components/backgroundImg.js
+++ frontend/src/components/backgroundImg.js
@@ -1,28 +1,20 @@
 //frontend/src/components/backgroundImg.js
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 
-function Img() {
-    const [loaded, setLoaded] = useState(false);
+const BackgroundImg = () => {
+  return (
+    <div className="img-container back">
+      <Image
+        src="/hotel.jpg"
+        alt="background Picture"
+        width={500}
+        height={500}
+        className="lazy-loaded-image"
+      />
+    </div>
+  );
+};
 
-    const handleLoadingComplete = () => {
-        setLoaded(true);
-    };
+export default BackgroundImg;
 
-    return (
-        <div className={loaded ? 'img-container loaded back' : 'img-container back'}>
-            <Image
-                src="hotel.jpg"
-                alt='background Picture'
-                width={500}
-                height={500}
-                onLoad={handleLoadingComplete}
-                className="lazy-loaded-image"
-                loaded="eager"
-
-            />
-        </div>
-    )
-}
-
-export default Img;
